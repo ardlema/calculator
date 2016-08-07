fd216595e1176621744c512ce44b859f2a8f69c3
Alberto Rodriguez <arodriguez@stratio.com>
2016-08-07 17:29:36 +0200

    Plus expressions with references

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -16,11 +16,17 @@ object Calculator {
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
     expr match {
+      case Literal(l) => l
       case Plus(a, b) => {
         (a, b) match {
           case (Literal(n1), Literal(n2)) => n1 + n2
-          case (expr1, Literal(n2)) => eval(expr1, references) + n2
+          case (Literal(n1), Ref(variable)) => n1 + eval(getReferenceExpr(variable, references), references)
+          case (Ref(variable), Literal(n1)) => eval(getReferenceExpr(variable, references), references) + n1
+          case (Ref(variable1), Ref(variable2)) =>
+            eval(getReferenceExpr(variable1, references), references) +
+              eval(getReferenceExpr(variable2, references), references)
           case (Literal(n1), expr2) => n1 + eval(expr2, references)
+          case (expr1, Literal(n2)) => eval(expr1, references) + n2
           case (expr1, expr2) => eval(expr1, references) + eval(expr2, references)
         }
       }
--- src/test/scala/calculator/CalculatorSuite.scala
+++ src/test/scala/calculator/CalculatorSuite.scala
@@ -28,5 +28,37 @@ class CalculatorSuite extends FunSuite with ShouldMatchers {
     val sum = Plus(expr1, c)
 
     assert(Calculator.eval(sum, Map()) == 9)
+
+    val a2 = Literal(2)
+    val b2 = Literal(3)
+    val c2 = Literal(4)
+    val expr2 = Plus(b2, c2)
+    val sum2 = Plus(a2, expr2)
+
+    assert(Calculator.eval(sum2, Map()) == 9)
+
+    val expr3 = Plus(sum, sum2)
+    assert(Calculator.eval(expr3, Map()) == 18)
+  }
+
+  test("eval Plus expression with references") {
+    val refB = Ref("b")
+    val a = Literal(1)
+    val expr = Plus(a, refB)
+    val signal = Signal[Expr](Literal(4))
+    val references = Map(("b", signal))
+
+    assert(Calculator.eval(expr, references) == 5)
+
+    val expr2 = Plus(refB, a)
+
+    assert(Calculator.eval(expr2, references) == 5)
+
+    val refC = Ref("c")
+    val signal2 = Signal[Expr](Literal(3))
+    val references2 = Map(("b", signal), ("c", signal2))
+    val expr3 = Plus(refB, refC)
+    assert(Calculator.eval(expr3, references2) == 7)
+
   }
 }
